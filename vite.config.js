c3f4e445032b3678cbfec6ccc1ba347efddc3958
evergared <evergared@gmail.com>
2023-07-04 12:57:27 +0700

    expose vite ke lan

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,14 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server :{
+        host : true
+    },
     plugins: [
         laravel({
             input: [
+                'resources/css/sb-admin.css',
+                'resoureces/css/absensi-dinas.js',
                 'resources/sass/app.scss',
                 'resources/js/app.js',
             ],
